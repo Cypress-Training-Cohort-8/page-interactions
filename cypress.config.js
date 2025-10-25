45caa96c1e9baa77346f2afed78d733173af26cb
Ademola Bhadmus <bhadmusautomates1@gmail.com>
2025-10-25 18:03:30 +0100

    shadow-dom feature

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
     viewportWidth: 1530,
     defaultCommandTimeout: 10000,
     chromeWebSecurity: false,
+    includeShadowDom: true,
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
